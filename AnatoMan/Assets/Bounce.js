b4e19889fe8c10772fa20ac0d545dc0e87914761
davidjllo <davidjllo@gmail.com>
2014-04-22 09:55:36 -0500

    Sprint 1

---
+++ AnatoMan/Assets/Bounce.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var displacement: int=2500;
+
+
+private var p0 : Vector3;
+
+function Start () {
+ p0= transform.position;
+}
+
+function Update () {
+
+
+rigidbody2D.velocity.x = 0;
+rigidbody2D.velocity.y = 0;
+transform.position= p0;
+
+}
+
+function OnCollisionEnter2D (colInfo : Collision2D) {
+	colInfo.collider.rigidbody2D.AddForce(new Vector2(0,displacement));
+	//colInfo.collider.gameObject.animation.Play("Jump");
+	
+	
+	//animation1.SetTrigger("PlatformPressed");
+	//rigidbody2D.AddForce(new Vector2(0,-0.05));
+	//animation1.Play("Platform");
+	
+}
---
+++ AnatoMan/Assets/Follow2D.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var character :Transform;
+function Start () {
+
+}
+
+function Update () {
+transform.position.x = character.position.x+7;
+}
